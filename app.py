0ca80c2f9db3275e913902f4fd28151c8b9f280a
bonlv2000 <bonhetco34@gmail.com>
2023-09-26 10:36:50 +0700

    this is failed dockerfile

--- app.py
+++ app.py
@@ -4,5 +4,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "<h1 style='text-align: center;'>Welcome to my Cloud DevOps Engineer Capstone Project!</h1>"
+
+    it is failed
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
